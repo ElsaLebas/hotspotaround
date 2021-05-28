ae4b2ad82e14b854b9e675abdf8120470e5cf402
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-28 12:34:13 +0200

    add the adress translate to lat and long

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,11 @@
 class Spot < ApplicationRecord
   has_many :favorites
+
   validates :title, presence: true
   validates :description, presence: true
   validates :address, presence: true
+
+  # geocoder
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
