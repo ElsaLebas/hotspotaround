ceb588f434eaff6d3f368cdf76d60f3648d34988
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-28 12:26:29 +0200

    implement simple_form for new spot

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -6,4 +6,16 @@ class SpotsController < ApplicationController
   def new
     @spot = Spot.new
   end
+
+  def create
+    @spot = Spot.new(spot_params)
+    @spot.save
+    redirect_to "/"
+  end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:title, :address, :description)
+  end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,6 +3,4 @@ class Spot < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :address, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
 end
