029f6340d043d79ea93172196e5d651c91bfdf26
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-28 13:35:52 +0200

    set up mapbox

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,6 +1,14 @@
 class SpotsController < ApplicationController
   def index
     @spots = Spot.all
+
+    # the `geocoded` scope filters only spots with coordinates (latitude & longitude)
+    @markers = @spots.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
   end
 
   def new
